ab58a4b02bd89ebdd8abf1a7a37f5195c9bf1069
Goldwinn <goldwinhalim770@gmail.com>
2024-12-10 11:06:12 +0700

    Model User

---
+++ app/src/main/java/com/example/alpvp/model/Users.kt
@@ -0,0 +1,10 @@
+package com.example.alpvp.model
+
+data class Users (
+    val user_id: Int = 0,
+    val username: String = "",
+    val email: String = "",
+    val password: String = "",
+    val profile_image: String = "",
+    val library_recipe_id: Int = 0,
+)
