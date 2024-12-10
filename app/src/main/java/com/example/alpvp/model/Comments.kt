8bd34de788e214738978d2a30c40a28bf2f345f1
Dannprand <153193400+Dannprand@users.noreply.github.com>
2024-12-10 11:34:38 +0700

    bikin comment libraryrecipes likes

---
+++ app/src/main/java/com/example/alpvp/model/Comments.kt
@@ -0,0 +1,8 @@
+package com.example.alpvp.model
+
+data class Comments(
+    val comment_id: Int = 0,
+    val content: String = "",
+    val post_id: Int = 0,
+    val user_id: Int = 0
+)
---
+++ app/src/main/java/com/example/alpvp/model/LibraryRecipes.kt
@@ -0,0 +1,8 @@
+package com.example.alpvp.model
+
+data class LibraryRecipes(
+    val library_recipe_id: Int = 0,
+    val title: String = "",
+    val caption: String = "",
+    val image_url: String = ""
+)
